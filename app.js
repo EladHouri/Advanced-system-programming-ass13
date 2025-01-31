7193e365f680d3743f9bfc83ad03e3223c129d99
EladHouri <eladyes640@gmail>
2025-01-31 10:13:23 +0200

    c2

--- app.js
+++ app.js
@@ -1,69 +1,40 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
+const session = require('express-session');
+
 const app = express();
-app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.static('public'));
-const jwt = require("jsonwebtoken");
-const key = "Some super secret key shhhhhhhhhhhhhhhhh!!!!!";
 
-const session = require('express-session');
 app.use(session({
-secret: 'foo',
-saveUninitialized: false,
-resave: false
-}))
+    secret: 'foo',
+    saveUninitialized: false,
+    resave: false,
+}));
 
 const index = (req, res) => {
-    res.json({ data: 'secret data' });
+    res.json({ data: 'Secret data: Only logged-in users can see this!' });
 };
 
-const isLoggedIn = (req, res, next) => {
-    if (req.headers.authorization) {
-        const token = req.headers.authorization.split(" ")[1];
-        try {
-            const data = jwt.verify(token, key);
-            console.log('The logged in user is: ' + data.username);
-            return next();
-        } catch (err) {
-            return res.status(401).json({ error: "Invalid Token" });
-        }
+function isLoggedIn(req, res, next) {
+    if (req.session.username) {
+        return next();
     } else {
-        return res.status(403).json({ error: 'Token required' });
+        res.redirect('/');
     }
-};
+}
 
 const processLogin = (req, res) => {
-    let attempts = 0;
-
-    if (req.cookies.attempts) {
-        attempts = parseInt(req.cookies.attempts);
-    }
-
-    if (attempts >= 3) {
-        return res.status(400).json({ error: 'Too many failed attempts.' });
-    }
-
     if (req.body.username === 'guest' && req.body.password === '123456') {
-        const data = { username: req.body.username };
-        const token = jwt.sign(data, key);
-        res.cookie('attempts', 0, { 
-            httpOnly: true, 
-            SameSite: 'None',
-        });
-        res.status(201).json({ token });
+        req.session.username = req.body.username;
+        res.status(200).json({ message: 'Login successful' });
     } else {
-        res.cookie('attempts', attempts + 1, { 
-            httpOnly: true, 
-            maxAge: 3600000, 
-            SameSite: 'None', 
-            Secure: true 
-        });
-        res.status(404).json({ error: 'Invalid username and/or password' });
+        res.status(401).json({ error: 'Invalid username and/or password' });
     }
 };
 
 app.post('/login', processLogin);
-app.get('/', isLoggedIn, index);
+app.get('/articles', isLoggedIn, index);
 
-app.listen(3200);
+app.listen(3200, () => {
+});
